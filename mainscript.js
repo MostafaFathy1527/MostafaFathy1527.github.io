f1e57f6882ed9799af4f5e4695c8a0e475e427af
mostafa <mostafafathy150@gmail.com>
2023-05-11 14:11:10 +0200

    Final

--- mainscript.js
+++ mainscript.js
@@ -2,5 +2,5 @@ const menuToggle = document.querySelector('.menu-toggle');
 const menuContainer = document.querySelector('.menu-container');
 
 menuToggle.addEventListener('click', () => {
-  menuContainer.classList.toggle('active');
+  menuContainer.style.display = menuContainer.style.display === 'block' ? 'none' : 'block';
 });
